11eb47875e83f92fafbd414e915745b5392425c1
Sergey Antonov <superflasher22@gmail.com>
2020-10-01 10:27:59 +0300

    Update build

---
+++ build/precache-manifest.b3e46fcc0db70d8e55efafd74003e0e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cd2ec9edcf5440661b2b8f98cd31b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac54672ee64d90fd731a",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "08b0ef46f8bac31cc30a",
+    "url": "./static/js/2.3b3ef6f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3b3ef6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac54672ee64d90fd731a",
+    "url": "./static/js/main.a7c8c063.chunk.js"
+  },
+  {
+    "revision": "f10db09fa13dacee5a65",
+    "url": "./static/js/runtime-main.abe83f61.js"
+  },
+  {
+    "revision": "108cbd14f87e9a4806dea1ef71c39835",
+    "url": "./static/media/logo.108cbd14.svg"
+  },
+  {
+    "revision": "6f30ca93b14917065e9c2bb49f120fc9",
+    "url": "./static/media/person.6f30ca93.svg"
+  },
+  {
+    "revision": "db5d8122836517965b6e0247a3a9691c",
+    "url": "./static/media/search.db5d8122.svg"
+  }
+]);
